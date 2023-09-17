25103e8372a5f813c094ec2d6a12b2aace400294
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-09-17 15:51:08 -0300

    Iniciando projeto

---
+++ orientacao-objetos/src/main/java/com/example/orientacaoobjetos/OrientacaoObjetosApplication.java
@@ -0,0 +1,13 @@
+package com.example.orientacaoobjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientacaoObjetosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrientacaoObjetosApplication.class, args);
+    }
+
+}
