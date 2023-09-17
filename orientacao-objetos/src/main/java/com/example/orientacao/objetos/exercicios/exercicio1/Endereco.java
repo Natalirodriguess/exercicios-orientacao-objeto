ec5ef4b75dc7da5929994c037fb841267571b7f4
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-09-17 16:19:12 -0300

    [feature-001] Gerando os getters e setters das classes Pessoa e Endereço

--- orientacao-objetos/src/main/java/com/example/orientacao/objetos/exercicios/exercicio1/Endereco.java
+++ orientacao-objetos/src/main/java/com/example/orientacao/objetos/exercicios/exercicio1/Endereco.java
@@ -4,4 +4,27 @@ public class Endereco {
     private String rua;
     private Integer numero;
     private String bairro;
+
+    private void setRua(String rua){
+        this.rua = rua;
+    }
+
+    private String getRua(){
+        return rua;
+    }
+
+    private void setNumero(){
+        this.numero = numero;
+    }
+
+    private Integer getNumero(){
+        return numero;
+    }
+
+    private void setBairro(){
+        this.bairro = bairro;
+    }
+    private String getBairro(){
+        return  bairro;
+    }
 }
--- orientacao-objetos/src/main/java/com/example/orientacao/objetos/exercicios/exercicio1/Pessoa.java
+++ orientacao-objetos/src/main/java/com/example/orientacao/objetos/exercicios/exercicio1/Pessoa.java
@@ -6,5 +6,35 @@ public class Pessoa {
     private Integer idade;
     private String cpf;
 
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 
+    public String nome(){
+        return nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
