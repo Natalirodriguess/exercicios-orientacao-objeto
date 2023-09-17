82889d38af7d35de35c28e0136e157a9ae05d299
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-09-17 16:05:37 -0300

    ajustes

--- orientacao-objetos/src/main/java/com/example/orientacao/objetos/OrientacaoObjetosApplication.java
+++ orientacao-objetos/src/main/java/com/example/orientacao/objetos/OrientacaoObjetosApplication.java
@@ -1,4 +1,4 @@
-package com.example.orientacaoobjetos;
+package com.example.orientacao.objetos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
